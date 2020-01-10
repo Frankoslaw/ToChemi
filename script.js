7b18ceaa715d93e08e067d89836aca2d7e299ca0
Frankoslaw <32892548+Frankoslaw@users.noreply.github.com>
2020-01-10 17:23:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var myDATA;
+var x = document.getElementById("text_box1");
 
-$.getJSON( "https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json", function( data ) {
-  myDATA = data;
-});
-
-console.log(myDATA);
+function run()
+{
+  if(x != "")
+  {
+    $.getJSON( "https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json", function( data ) {
+    });
+  }else
+  {
+    console.log("Podaj dane!");
+    //tutaj dam zwrot błędu
+  }
+}
