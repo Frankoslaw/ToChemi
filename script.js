0b7d8631063de48fab8f9428953420b900e787ff
Frankoslaw <32892548+Frankoslaw@users.noreply.github.com>
2020-01-10 18:08:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function run()
     else
     {
       $.getJSON( "https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json", function( data ) {
-        for(var i = data["elements"].length;i > 1;i--)
+        for(var i = data["elements"].length;i > 0;i--)
         {
-          console.log(x.toUpperCase() + " | " + data["elements"][i - 1]["symbol"]);
-          if(x.toUpperCase() == data["elements"][i - 1]["symbol"])
+          console.log(x + " | " + data["elements"][i - 1]["symbol"]);
+          if(x == data["elements"][i - 1]["symbol"])
           {
-            console.log("Masa atomowa" + Math.round(data["elements"][i - 1]["atomic_mass"]));
-            console.log("Elektrony i protony" + data["elements"][i - 1]["number"]);
+            console.log("Masa atomowa" + Math.round(data["elements"][i]["atomic_mass"]));
+            console.log("Elektrony i protony" + data["elements"][i]["number"]);
           }
         }
       });
