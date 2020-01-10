e58ca1126bac812b8177f9bfe8bce4d3073b1585
Frankoslaw <32892548+Frankoslaw@users.noreply.github.com>
2020-01-10 17:03:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var network_data;
-$.getJSON("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json", 
-function(data){
-    network_data = data.contents;
-})
-
-console.log(network_data["elements"].length);
+$.getJSON( "https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json", function( data ) {
+  console.log(data);
+});
